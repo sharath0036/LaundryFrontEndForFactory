edc7c6f404c165df27a216d3d038ff0cf631cce2
sharath0036 <ksharathbabu2@gmail.com>
2024-11-19 17:27:10 +0530

    formmater fixed

--- FromBranch/FromBranchVoucher/Js/pages/InsertScan/FormLoad/BuildBsTable/ForColumns/entryFile.js
+++ FromBranch/FromBranchVoucher/Js/pages/InsertScan/FormLoad/BuildBsTable/ForColumns/entryFile.js
@@ -1,10 +1,28 @@
 import { StartFunc as ColumnOperate } from "./ColumnOperate/entryFile.js";
+import { StartFunc as SentFromBranch } from "./SentFromBranch/entryFile.js";
+import { StartFunc as Scanned } from "./Scanned/entryFile.js";
+import { StartFunc as Pending } from "./Pending/entryFile.js";
+
+
+
 
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
     let LocalColumnOperateFine = LocalColumns.find(element => element.field === "operate");
 
     ColumnOperate({ inFindColumn: LocalColumnOperateFine });
+
+    let LocalColumnSentFromBranch= LocalColumns.find(element => element.field === "Sent");
+
+    SentFromBranch({ inFindColumn: LocalColumnSentFromBranch });
+
+    let LocalColumnScanned= LocalColumns.find(element => element.field === "scanned");
+
+    Scanned({ inFindColumn: LocalColumnScanned });
+
+    let LocalColumnPending= LocalColumns.find(element => element.field === "Pending");
+
+    Pending({ inFindColumn: LocalColumnPending });
 };
 
 export { StartFunc };
