d9454831cc97141914d00dfa486d5314d67646ca
Nakka Lakshmi Chandana <lakshmichandananakka05@gmail.com>
2024-11-22 19:11:06 +0530

    Filters

---
+++ AtFactory/CompletionQrCodes/Js/pages/ScannedFilterShow/FormLoad/BuildBsTable/EntryFile.js
@@ -0,0 +1,19 @@
+import optionsJson from './options.json' with {type: 'json'};
+import { StartFunc as ForColumns } from "./ForColumns/entryFile.js";
+import { StartFunc as onClickRow } from "./onClickRow/EntryFile.js";
+import { StartFunc as queryParams } from "./queryParams/EntryFile.js";
+import { StartFunc as responseHandler } from "./responseHandler/EntryFile.js";
+
+const StartFunc = () => {
+    var $table = $('#table');
+
+    optionsJson.onClickRow = onClickRow;
+    ForColumns({ inColumns: optionsJson.columns });
+    optionsJson.queryParams = queryParams;
+    optionsJson.responseHandler = responseHandler;
+    let LocalFactoryName = localStorage.getItem("FactoryName");
+    optionsJson.url=`/Custom/Clients/Laundry/Factory/Outward/Scan/QrCode/Scanned/${LocalFactoryName}`;
+    $table.bootstrapTable(optionsJson);
+};
+
+export { StartFunc };
