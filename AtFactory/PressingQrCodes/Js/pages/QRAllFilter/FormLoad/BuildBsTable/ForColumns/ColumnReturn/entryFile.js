7501fdad9c35c82a7fe6353a9c49d3b1bcbd5ac2
Nakka Lakshmi Chandana <lakshmichandananakka05@gmail.com>
2024-11-25 16:05:07 +0530

    pagination

--- AtFactory/PressingQrCodes/Js/pages/QRAllFilter/FormLoad/BuildBsTable/ForColumns/ColumnReturn/entryFile.js
+++ AtFactory/PressingQrCodes/Js/pages/QRAllFilter/FormLoad/BuildBsTable/ForColumns/ColumnReturn/entryFile.js
@@ -5,9 +5,9 @@ let StartFunc = ({ inFindColumn }) => {
 let jVarLocalFormatterFunc = (value, row, index) => {
     console.log("value : ", value);
     if (value === true) {
-        return '<span class="badge bg-success">Scanned</span>';
+        return '<span class="badge bg-success">Returned</span>';
     } else {
-        return '<span class="badge bg-danger">Pending</span>';
+        return 'Instock';
     };
 };
 
