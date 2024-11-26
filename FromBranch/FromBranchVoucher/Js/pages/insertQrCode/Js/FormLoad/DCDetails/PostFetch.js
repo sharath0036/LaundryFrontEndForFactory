62bbf3db0ee54a9e9cc698213e7178584c5f26c8
Srivasu215 <sri.vasu215@gmail.com>
2024-11-26 15:41:21 +0530

    ENTRYSCAN

---
+++ FromBranch/FromBranchVoucher/Js/pages/insertQrCode/Js/FormLoad/DCDetails/PostFetch.js
@@ -0,0 +1,18 @@
+let StartFunc = async () => {
+    let jVarLocalFilterString = getUrlQueryParams({ inGetKey: "VoucherRef" });
+
+    let jVarLocalFetchUrl = `/Custom/Cleaning/Factory/V1/Inward/Scan/Voucher/RowData/${jVarLocalFilterString}`;
+    let response = await fetch(jVarLocalFetchUrl);
+    let data = await response.json();
+
+    return await data;
+};
+
+let getUrlQueryParams = ({ inGetKey }) => {
+    const queryString = window.location.search;
+    const parameters = new URLSearchParams(queryString);
+    const value = parameters.get(inGetKey);
+    return value;
+};
+
+export { StartFunc };
