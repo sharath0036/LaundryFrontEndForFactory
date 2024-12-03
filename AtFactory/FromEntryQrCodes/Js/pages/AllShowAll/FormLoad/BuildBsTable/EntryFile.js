0a5056929ad2430bfd89ba9475faf93e0524f019
sharath0036 <ksharathbabu2@gmail.com>
2024-12-03 12:57:15 +0530

    end points fixed

--- AtFactory/FromEntryQrCodes/Js/pages/AllShowAll/FormLoad/BuildBsTable/EntryFile.js
+++ AtFactory/FromEntryQrCodes/Js/pages/AllShowAll/FormLoad/BuildBsTable/EntryFile.js
@@ -12,7 +12,7 @@ const StartFunc = () => {
     optionsJson.queryParams = queryParams;
     optionsJson.responseHandler = responseHandler;
     let LocalFactoryName = localStorage.getItem("FactoryName");
-    optionsJson.url=`/Custom/Clients/Laundry/Factory/Process/Washing/Scan/QrCode/${LocalFactoryName}`;
+    optionsJson.url=`/Custom/Cleaning/Factory/V1/Process/Washing/Scan/QrCode/${LocalFactoryName}`;
     $table.bootstrapTable(optionsJson);
 };
 
