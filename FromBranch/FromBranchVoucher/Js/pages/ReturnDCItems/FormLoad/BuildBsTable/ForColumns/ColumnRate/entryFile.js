4b83941f72062ddb032e21df45c9113bf663db6a
kavycoder <nallakavya248@gmail.com>
2025-01-04 19:15:55 +0530

    sent formater from html to js

---
+++ FromBranch/FromBranchVoucher/Js/pages/ReturnDCItems/FormLoad/BuildBsTable/ForColumns/ColumnRate/entryFile.js
@@ -0,0 +1,20 @@
+const StartFunc = ({ inFindColumn }) => {
+    inFindColumn.formatter = symbolFormatPrice;
+    inFindColumn.footerFormatter  = priceFormatter;
+};
+
+function priceFormatter(data) {
+    var field = this.field
+    return '₹ ' + data.map(function (row) {
+        return +row.Rate
+    }).reduce(function (sum, i) {
+        return sum + i
+    }, 0)
+}
+function symbolFormatPrice(data) {
+    var field = this.field
+    return '₹ '+data;
+
+ }
+
+export { StartFunc };
