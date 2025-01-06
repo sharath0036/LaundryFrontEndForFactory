c6070945712fcb668ea72249301c1d65c20f11c9
kavycoder <nallakavya248@gmail.com>
2025-01-06 15:33:35 +0530

    changed insertto returndc functns to js files

---
+++ FromBranch/FromBranchVoucher/Js/pages/insertToReturnDC/FormLoad/BuildBsTable/ForColumns/ColumnItems/entryFile.js
@@ -0,0 +1,17 @@
+const StartFunc = ({ inFindColumn }) => {
+    
+    inFindColumn.footerFormatter  = priceFormatter;
+};
+
+function priceFormatter(data) {
+    var field = this.field
+    return '' + data.map(function (row) {
+        return +row.ItemDetails
+    }).reduce(function (sum, i) {
+        return sum + i
+    }, 0)
+}
+
+
+
+export { StartFunc };
