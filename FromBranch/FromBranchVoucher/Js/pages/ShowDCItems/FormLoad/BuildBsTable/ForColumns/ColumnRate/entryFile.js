18e502ea74fe6ca4013e86b6348434b28458faaf
kavycoder <nallakavya248@gmail.com>
2025-01-06 13:41:50 +0530

    moved showdcitems funcs to js files

--- FromBranch/FromBranchVoucher/Js/pages/ShowDCItems/FormLoad/BuildBsTable/ForColumns/ColumnRate/entryFile.js
+++ FromBranch/FromBranchVoucher/Js/pages/ShowDCItems/FormLoad/BuildBsTable/ForColumns/ColumnRate/entryFile.js
@@ -1,16 +1,16 @@
 const StartFunc = ({ inFindColumn }) => {
     inFindColumn.formatter = symbolFormatPrice;
-    inFindColumn.footerFormatter  = totalFormatter;
+    inFindColumn.footerFormatter  = priceFormatter;
 };
 
-function totalFormatter(data) {
-    var field = this.field;
+function priceFormatter(data) {
+    var field = this.field
     return '₹ ' + data.map(function (row) {
         return +row.Rate
     }).reduce(function (sum, i) {
         return sum + i
     }, 0)
-};
+}
 function symbolFormatPrice(data) {
     var field = this.field
     return '₹ ' + data;
