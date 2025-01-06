6f0778835a195871e59f3dce7cd92718f4992b7b
kavycoder <nallakavya248@gmail.com>
2025-01-06 12:50:37 +0530

    moved statusdc js code to js files

---
+++ FromBranch/FromBranchVoucher/Js/pages/StatusDC/FormLoad/BuildBsTable/ForColumns/ColumnEntryCancel/entryFile.js
@@ -0,0 +1,20 @@
+const StartFunc = ({ inFindColumn }) => {
+    inFindColumn.formatter = totalFormatter;
+   
+};
+
+function totalFormatter(data) {
+    var field = this.field;
+    let LocalTotal = data.map(function (row) {
+        if (row[field] === undefined) {
+            return 0;
+        };
+        return +row[field];
+    }).reduce(function (sum, i) {
+        return sum + i
+    }, 0);
+
+    return Math.floor(LocalTotal);
+}
+
+export { StartFunc };
