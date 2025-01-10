cf8bab4ea66988085d1e68679d604b13ea6b1ccb
pushpadamalanka82 <y*********@gmail.com>
2025-01-10 18:15:37 +0500

    changes

--- FromBranch/FromBranchVoucher/Js/pages/ReturninsertQrCode/FormLoad/QrTable/Nav.js
+++ FromBranch/FromBranchVoucher/Js/pages/ReturninsertQrCode/FormLoad/QrTable/Nav.js
@@ -1,7 +1,7 @@
 let StartFunc = () => {
     const url = new URL(window.location.href);
     const params1 = new URLSearchParams(url.search);
-    let NewURl = new URL("./ReturnDCItems.html", url);
+    let NewURl = new URL("./ShowDCEntryCancel.html", url);
     const new_url = new URL(`${NewURl.href}?${params1}`);
     window.location.href = new_url.href;
 };
