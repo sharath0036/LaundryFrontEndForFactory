f76961a255c05bd9c94c3383dc4cbb3ce290d369
Achansandeep21 <93124032+Achansandeep21@users.noreply.github.com>
2025-01-10 14:57:52 +0530

    REWASH STATUS

--- AtFactory/PressingQrCodes/Js/pages/QRReWashFilter/FormLoad/BuildBsTable/ForColumns/EntryFile.js
+++ AtFactory/PressingQrCodes/Js/pages/QRReWashFilter/FormLoad/BuildBsTable/ForColumns/EntryFile.js
@@ -1,16 +1,23 @@
 import { StartFunc as ColumnOperate } from "./ColumnSerial/entryFile.js";
 import { StartFunc as ColumnRate } from "./ColumnRate/entryFile.js";
+import { StartFunc as ColumnReWash } from "./ColumnReWash/entryFile.js";
+
 
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
     let LocalColumnOperateFine = LocalColumns.find(element => element.field === "KS-Serial")
     let LocalColumnRate = LocalColumns.find(element => element.field === "Rate");
+    let LocalColumnRewash = LocalColumns.find(element => element.field === "ReWash");
+
 
     if (LocalColumnOperateFine === undefined === false) {
         ColumnOperate({ inFindColumn: LocalColumnOperateFine });
     };
     if (LocalColumnRate === undefined === false) {
         ColumnRate({ inFindColumn: LocalColumnRate });
+    };
+    if (LocalColumnRewash === undefined === false) {
+        ColumnReWash({ inFindColumn: LocalColumnRewash });
     };
 };
 export { StartFunc };
