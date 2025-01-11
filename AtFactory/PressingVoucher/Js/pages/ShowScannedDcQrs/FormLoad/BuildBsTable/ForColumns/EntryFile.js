5217163e0b46ee0207ba8130777ecc3da913ff09
Achansandeep21 <93124032+Achansandeep21@users.noreply.github.com>
2025-01-11 18:08:44 +0530

    script is remove from html's

--- AtFactory/PressingVoucher/Js/pages/ShowScannedDcQrs/FormLoad/BuildBsTable/ForColumns/EntryFile.js
+++ AtFactory/PressingVoucher/Js/pages/ShowScannedDcQrs/FormLoad/BuildBsTable/ForColumns/EntryFile.js
@@ -1,10 +1,21 @@
 import { StartFunc as ColumnRewash } from "./ColumnRewash/entryFile.js";
+import { StartFunc as ColumnSerial } from "./ColumnSerial/entryFile.js";
+import { StartFunc as ColumnRate } from "./ColumnRate/entryFile.js";
+
 
 let StartFunc = ({ inColumns }) => {
     let LocalColumns = inColumns;
+    let LocalColumnSerial = LocalColumns.find(element => element.field === "KS-Serial");
+    let LocalColumnRate = LocalColumns.find(element => element.field === "Rate");
     let LocalColumnRewash = LocalColumns.find(element => element.field === "Rewash");
 
 
+    if (LocalColumnSerial === undefined === false) {
+        ColumnSerial({ inFindColumn: LocalColumnSerial });
+    };
+    if (LocalColumnRate === undefined === false) {
+        ColumnRate({ inFindColumn: LocalColumnRate });
+    };
     if (LocalColumnRewash === undefined === false) {
         ColumnRewash({ inFindColumn: LocalColumnRewash });
     };
